78d45bda98f7ecd2e02d72d5355ff545b803330e
NoamDev <37066741+NoamDev@users.noreply.github.com>
2018-06-29 18:22:03 +0300

    client edit

--- client/game.js
+++ client/game.js
@@ -1,1 +1,21 @@
-
+JSG.Game = class
+  {
+    //set the tag which the game's canvas should be in.
+   setContainer(tag)
+    {
+      tag.appendChild(canvas);
+    }
+    
+   //constructor(), constructor(container), constructor(height, width) or constructor(container,height,width)
+   constructor()
+    {
+      this.canvas=document.createElement("canvas");      
+      switch(arguments.length)
+      {
+        case 1://sent only container
+          setContainer(arguments[0])
+          break;
+      }
+    }
+    
+  }
--- client/lib.js
+++ client/lib.js
@@ -1,24 +1,30 @@
-var JSG =
+window.JSG=(typeof JSG !== 'undefined'?JSG:{});
+    // extract filename and callback parameter from the URL
+    (function(){
+	var matches = document.currentScript.src.match(/callback=([^&]*)(&.*|$)/);
+        // call the callback function
+        JSG.callback=((matches!=null)?matches[1]:null);
+	})();
+JSG.h={}//help fuctions
+JSG.h.loadScript=function(url, callback)
+
 {
-  Game:class
-  {
-    //set the tag which the game's canvas should be in.
-   setContainer(tag)
-    {
-      tag.appendChild(canvas);
-    }
-    
-   //constructor(), constructor(container), constructor(height, width) or constructor(container,height,width)
-   constructor()
-    {
-      this.canvas=document.createElement("canvas");      
-      switch(arguments.length)
-      {
-        case 1://sent only container
-          setContainer(arguments[0])
-          break;
-      }
-    }
-    
-  }
-};
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+JSG.h.loadScript("game.js",function(e){
+	if(JSG.callback!=null)
+		(window[JSG.callback])()
+});
