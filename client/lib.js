3d05467dcdaf7b09c803f5b9ab66f29ae4b1ab7c
NoamDev <37066741+NoamDev@users.noreply.github.com>
2018-06-29 12:23:28 +0300

    Update lib.js

--- client/lib.js
+++ client/lib.js
@@ -2,6 +2,14 @@ var JSG =
 {
   Game:class
   {
-    
+    constructor()
+    {
+      this.container=null;
+      this.canvas=document.createElement("canvas");
+    }
+    setContainer(tag)
+    {
+      
+    }
   }
 };
