f7aae46b85feb9e627df57d69305202a439f98e0
NoamDev <noam.yem@gmail.com>
2018-06-30 23:48:38 +0300

    my first commit

--- client/game.js
+++ client/game.js
@@ -5,17 +5,31 @@ JSG.Game = class
     {
       tag.appendChild(canvas);
     }
-    
+   setDimentions(height,width)
+   {
+	   this.canvas.height=height;
+	   this.canvas.width=width;
+   }   
    //constructor(), constructor(container), constructor(height, width) or constructor(container,height,width)
    constructor()
     {
       this.canvas=document.createElement("canvas");      
       switch(arguments.length)
       {
-        case 1://sent only container
+		case 1://sent only container.
           setContainer(arguments[0])
-          break;
+		  break;
+		case 2://sent only height and width.
+		  setDimentions(arguments[0],arguments[1]);
+		  break;
+		 case 3://sent both container and height,width
+  		  setDimentions(arguments[1],arguments[2]);
+		  setContainer(arguments[0]);
       }
     }
+	start()
+	{
+		
+	}
     
   }
