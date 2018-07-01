1c73b9683e3aecd45fd1b9329950470ae4125bab
NoamDev <noam.yem@gmail.com>
2018-07-01 19:19:36 +0300

    added class sprite

---
+++ client/sprite.js
@@ -0,0 +1,28 @@
+JSG.Sprite = class
+  {  
+   constructor()//can be called with url or img too.
+    {
+		this.img=document.createElement('img');
+		if(argument.length >= 0)
+			if(arguments[0] instanceof Image)
+				this.img=arguments[0];
+			else
+				this.img.src = arguments[0];
+    }
+	set img(img)
+	{
+		this.img=img;
+	}
+	get img()
+	{
+		return this.img;
+	}
+	set url(url)
+    {
+		this.img.src=url;
+	}
+	get url()
+	{
+		return this.img.src;
+	}
+  }
